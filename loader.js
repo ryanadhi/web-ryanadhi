6ac9d0ab54a7f0255e3496b87f616374a1ed0159
ryanadhi.p <ryanadhi.p@gmail.com>
2024-07-10 03:43:12 +0000

    feat: project-idx

--- loader.js
+++ loader.js
@@ -1,5 +1,8 @@
 "use client";
 
+// export default function myImageLoader({ src, width, quality }) {
+//   return `https://example.com/${src}?w=${width}&q=${quality || 75}`;
+// }
 export default function myImageLoader({ src, width, quality }) {
-  return `https://example.com/${src}?w=${width}&q=${quality || 75}`;
+  return `` + src + `?w=${width}&q=${quality || 75}`; 
 }
